756d20452d4d5940b765e7d2c9ed20acd7b35aa8
Josh Dassinger <dassjosh@users.noreply.github.com>
2024-11-02 09:21:23 -0500

    Better implementation deserialization of RoleTags

---
+++ Oxide.Ext.Discord/Json/Converters/NullBoolConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Oxide.Ext.Discord.Json
+{
+    internal class NullBoolConverter : JsonConverter
+    {
+        public override bool CanWrite => false;
+        
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotImplementedException();
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) => true;
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(bool);
+    }
+}
