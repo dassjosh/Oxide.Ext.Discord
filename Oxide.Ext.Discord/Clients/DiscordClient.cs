425bd4efe2633f7a9e671c807ad2eb03fc218271
Josh Dassinger <dassjosh@users.noreply.github.com>
2024-11-24 12:24:43 -0600

    Cherry-Pick conflicts

--- Oxide.Ext.Discord/Clients/DiscordClient.cs
+++ Oxide.Ext.Discord/Clients/DiscordClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Newtonsoft.Json;
 using Oxide.Core.Plugins;
 using Oxide.Ext.Discord.Connections;
